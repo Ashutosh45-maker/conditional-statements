011e0d5a18e93e0e8f4fd52e6c2de2453301bcc7
Ashutosh45-maker <ashutoshparadkar05@gmail.com>
2024-10-06 21:22:53 +0530

    conditional statements

---
+++ main.py
@@ -0,0 +1,57 @@
+# age=int(input("enter your age"))
+# if age>=18:
+#     print("allowed")
+# else:
+#     print("not allowed")
+
+    #activity 2
+# number=int(input("enter your number:"))
+# if number>0:
+#   print("the number is positive")
+# else:
+#    print("the number is negative")
+
+# #activity 3
+# number=int(input("enter your number:"))
+# if number%2==0:
+#     print("the number is even number")
+# else:
+#     print("the number is odd number")
+
+#activity 4
+# height=int(input("enter your height in cm:"))
+# weight=int(input("enter your weight in kg:"))
+
+# bmi=weight/(height/100)**2
+# print(bmi)
+# if bmi<=18.4:
+#     print("you are underweight")
+# elif bmi<=24.9:
+#     print("you are healthy")
+# elif bmi<=29.9:
+#     print("you are overweight")
+# elif bmi<=34.9:
+#     print("you are severely overweight")
+# elif bmi<=39.9:
+#     print("you are obese")
+# else:
+#     print("you are severely obese")
+
+#activity 5
+# number=int(input("enter your number to check:"))
+# if number>50:
+#     print("number is greater than 50")
+#     if number%2==0:
+#         print("even number") 
+#     else:
+#         print("number is odd")
+# else:
+#     print("number is less than 50")
+
+#activity 6
+import datetime
+currenttime=datetime.datetime.now()
+print(currenttime)
+
+import calendar
+print(calendar.calendar(2024))
